b531769bf4f6b33d7d61cc8c88fae38bd4802e06
Joao Porfirio <joaoprf54@gmail.com>
2022-06-23 15:46:16 -0300

    Removendo formatacao

--- app.js
+++ app.js
@@ -191,21 +191,21 @@ app.get('/deputados/:id/gastosPorMes/:ano', (request, response) => {
 			valorFinalJul+valorFinalAgo+
 			valorFinalSet+valorFinalOut+
 			valorFinalNov+valorFinalDez;
-			
+
 			return response.send(
 				{
-					"janeiro": formatarMoeda(valorFinalJan),
-					"fevereiro": formatarMoeda(valorFinalFev),
-					"marco": formatarMoeda(valorFinalMar),
-					"abril": formatarMoeda(valorFinalAbr),
-					"maio": formatarMoeda(valorFinalMai),
-					"junho": formatarMoeda(valorFinalJun),
-					"julho": formatarMoeda(valorFinalJul),
-					"agosto": formatarMoeda(valorFinalAgo),
-					"setembro": formatarMoeda(valorFinalSet),
-					"outubro": formatarMoeda(valorFinalOut),
-					"novembro": formatarMoeda(valorFinalNov),
-					"dezembro": formatarMoeda(valorFinalDez),
+					"janeiro": valorFinalJan,
+					"fevereiro": valorFinalFev,
+					"marco":valorFinalMar,
+					"abril":valorFinalAbr,
+					"maio": valorFinalMai,
+					"junho": valorFinalJun,
+					"julho": valorFinalJul,
+					"agosto":valorFinalAgo,
+					"setembro": valorFinalSet,
+					"outubro": valorFinalOut,
+					"novembro": valorFinalNov,
+					"dezembro": valorFinalDez,
 					"totalAno": formatarMoeda(valorTotalAno)
 				}
 				);
